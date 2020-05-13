a662fbd0097fedd8d8bafd0e8eaa6e20c799a473
boosun13 <double0two22@gmail.com>
2020-05-13 17:57:20 +0900

    loop add

--- portfolioPage/ja/main.js
+++ portfolioPage/ja/main.js
@@ -1,21 +1,23 @@
 "use strict"
 {
-const images = ["img/img.1", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg"];
+const images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg"];
 // 画像の配列作成
 
 let i = 0;
 
 const changeImage = (num) => {
-    if(i + num >= 0 && i + num < images.length)
-    i += num;
-    document.getElementById("main_image").src = images[i]
-    img.classList.add("trans");
+    if(i + num >= 0 && i + num < images.length){
+        i += num;
+        document.getElementById("main_image").src = images[i]
+    }
 };
 
 const timer = setInterval(() => {
     changeImage(1);
     console.log(i);
-    if(i >= images.length-1) clearInterval(timer);
+    if(i >= images.length-1){
+        i = 0;
+    } ;
 }, 4000);
 
 
