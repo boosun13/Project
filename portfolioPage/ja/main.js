34555386c526f3fe8a43ffd934f99c3a172a133a
boosun13 <double0two22@gmail.com>
2020-05-17 01:05:52 +0900

    about hover add

--- portfolioPage/ja/main.js
+++ portfolioPage/ja/main.js
@@ -1,24 +1,3 @@
 "use strict"
-{
-const images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg"];
-// 画像の配列作成
-
-let i = 0;
-
-const changeImage = (num) => {
-    if(i + num >= 0 && i + num < images.length){
-        i += num;
-        document.getElementById("main_image").src = images[i]
-    }
-};
-
-const timer = setInterval(() => {
-    changeImage(1);
-    console.log(i);
-    if(i >= images.length-1){
-        i = 0;
-    } ;
-}, 10000);
 
 
-}
